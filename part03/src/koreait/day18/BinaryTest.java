2bb2caef1e7c711f21fc7055187385b480ed0c30
after <ovis2021@naver.com>
2022-06-22 10:04:39 +0900

    세번쨰 커밋

--- part03/src/koreait/day18/BinaryTest.java
+++ part03/src/koreait/day18/BinaryTest.java
@@ -12,3 +12,12 @@ public class BinaryTest {
 	}
 
 }
+/*	10진수 정수값을 다른 진법(진수)으로 문자열 변환
+ *	Integer.toBinaryString(i)	->	2진수로 변환
+ *	Integer.toOctalString(i)	->	8진수로 변환
+ *	Integer.toHexString(i)		->	16진수로 변환
+ *
+ *	int형식은 4바이트 32비트 2진수는 32자리, 16진수로는 8자리, 10진수로는 값 범위 -2147483648~2147483647
+ *												FFFFFFFF(32비트가 모두 1, 02147383648)
+ *	xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+*/
